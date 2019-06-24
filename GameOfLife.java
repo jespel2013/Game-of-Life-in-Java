dddefd51192bb16e076f91c9b88e7b21426cfaa2
jespel2013 <jespel2013@gmail.com>
2019-06-24 12:45:56 -0700

    Final Commit

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,3 +1,4 @@
+//Jesse Pelletier
 /**
  * The model for John Conway's Game of Life.
  *
@@ -8,124 +9,239 @@
  * @author Rick Mercer and Your Name
  */
 public class GameOfLife {
+	private int[][] grid;
+	private int rows;
+	private int cols;
 
-  /**
-   * Write the constructor so it takes two integer arguments to represent the
-   * number of rows and columns in the game of life. The constructor creates a
-   * society with no cells but space to store rows*cols cells.
-   *
-   * @param rows
-   *          The height of the grid that shows the cells.
-   * @param cols
-   *          The width of the grid that shows the cells.
-   */
-  public GameOfLife(int rows, int cols) {
-    // TODO: Complete this method
-  }
+	/**
+	 * Write the constructor so it takes two integer arguments to represent the
+	 * number of rows and columns in the game of life. The constructor creates a
+	 * society with no cells but space to store rows*cols cells.
+	 *
+	 * @param rows The height of the grid that shows the cells.
+	 * @param cols The width of the grid that shows the cells.
+	 */
+	public GameOfLife(int r, int c) {
+		// TODO: Complete this method
+		grid = new int[r][c];
+		rows = r;
+		cols = c;
 
-  /**
-   * Return the number of rows, which can be indexed from 0..numberOfRows()-1.
-   *
-   * @return The height of the society.
-   */
-  public int numberOfRows() {
-    // TODO: Complete this method
-    return Integer.MIN_VALUE;
-  }
+	}
 
-  /**
-   * The number of columns, which can be indexed from 0..numberOfColumns()-1.
-   *
-   * @return The height of the society.
-   */
-  public int numberOfColumns() {
-    // TODO: Complete this method
-    return Integer.MIN_VALUE;
-  }
+	/**
+	 * Return the number of rows, which can be indexed from 0..numberOfRows()-1.
+	 *
+	 * @return The height of the society.
+	 */
+	public int numberOfRows() {
+		// TODO: Complete this method
+		return rows;
+	}
 
-  /**
-   * Place a new cell in the society.
-   * 
-   * @param row
-   *          The row to grow the cell.
-   * @param col
-   *          The column to grow the cell.
-   *
-   *          Precondition: row and col are in range.
-   */
-  public void growCellAt(int row, int col) {
-    // TODO: Complete this method
-  }
+	/**
+	 * The number of columns, which can be indexed from 0..numberOfColumns()-1.
+	 *
+	 * @return The height of the society.
+	 */
+	public int numberOfColumns() {
+		// TODO: Complete this method
+		return cols;
+	}
 
-  /**
-   * 5) Return true if there is a cell at the given row and column. Return false
-   * if there is none at the specified location.
-   *
-   * @param row
-   *          The row to check.
-   * @param col
-   *          The column to check.
-   * @return True if there is a cell at the given row or false if none
-   *
-   *         Precondition: row and col are in range.
-   */
-  public boolean cellAt(int row, int col) {
-    // TODO: Complete this method
-    return ! true;
-  }
+	/**
+	 * Place a new cell in the society.
+	 * 
+	 * @param row The row to grow the cell.
+	 * @param col The column to grow the cell.
+	 *
+	 *            Precondition: row and col are in range.
+	 */
+	public void growCellAt(int row, int col) {
+		grid[row][col] = 1;
+	}
 
-  /**
-   * Return one big string of cells to represent the current state of the society
-   * of cells (see output below where '.' represents an empty space and 'O' is a
-   * live cell. There is no need to test toString. Simply use it to visually
-   * inspect if needed. Here is one sample output from toString:
-   *
-   * GameOfLife society = new GameOfLife(4, 14); society.growCellAt(1, 2);
-   * society.growCellAt(2, 3); society.growCellAt(3, 4);
-   * System.out.println(society.toString());
-   *
-   * @return A textual representation of this society of cells.
-   */
-  // Sample Output:
-  // ..............
-  // ..O...........
-  // ...O..........
-  // ....O.........
-  @Override
-  public String toString() {
-    // TODO: Complete this method
-    return "Under construction";
-  }
+	/**
+	 * 5) Return true if there is a cell at the given row and column. Return false
+	 * if there is none at the specified location.
+	 *
+	 * @param row The row to check.
+	 * @param col The column to check.
+	 * @return True if there is a cell at the given row or false if none
+	 *
+	 *         Precondition: row and col are in range.
+	 */
+	public boolean cellAt(int row, int col) {
+		// TODO: Complete this method
+		return grid[row][col] == 1;
+	}
 
-  /**
-   * The return values should always be in the range of 0 through 8.
-   *
-   * @return The number of neighbors around any cell using wrap around.
-   * 
-   * Precondition: row and col are in range.
-   *
-   * Count the neighbors around the given location. Use wraparound. A cell in row
-   * 0 has neighbors in the last row if a cell is in the same column, or the
-   * column to the left or right. In this example, cell 0,5 has two neighbors in
-   * the last row, cell 2,8 has four neighbors, cell 2,0 has four neighbors, cell
-   * 1,0 has three neighbors. The cell at 3,8 has 3 neighbors. The potential
-   * location for a cell at 4,8 would have three neighbors.
-   */
-  // .....O..O
-  // O........
-  // O.......O
-  // O.......O
-  // ....O.O..
-  public int neighborCount(int row, int col) {
-    // TODO: Complete this method
-    return Integer.MIN_VALUE;
-  }
+	/**
+	 * Return one big string of cells to represent the current state of the society
+	 * of cells (see output below where '.' represents an empty space and 'O' is a
+	 * live cell. There is no need to test toString. Simply use it to visually
+	 * inspect if needed. Here is one sample output from toString:
+	 *
+	 * GameOfLife society = new GameOfLife(4, 14); society.growCellAt(1, 2);
+	 * society.growCellAt(2, 3); society.growCellAt(3, 4);
+	 * System.out.println(society.toString());
+	 *
+	 * @return A textual representation of this society of cells.
+	 */
+	// Sample Output:
+	// ..............
+	// ..O...........
+	// ...O..........
+	// ....O.........
+	@Override
+	public String toString() {// to do
+		String gridStr = "";
 
-  /**
-   * Update the state to represent the next society. Typically, some cells will
-   * die off while others are born.
-   */
-  public void update() {
-    // TODO: Complete this method
-  }
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				if (grid[r][c] == 1) {
+					gridStr += "O";
+				} else {
+					gridStr += ".";
+				}
+			}
+			if (r != rows - 1) {
+				gridStr += "\n";
+			}
+		}
+
+		return gridStr;
+	}
+
+	/**
+	 * The return values should always be in the range of 0 through 8.
+	 *
+	 * @return The number of neighbors around any cell using wrap around.
+	 * 
+	 *         Precondition: row and col are in range.
+	 *
+	 *         Count the neighbors around the given location. Use wraparound. A cell
+	 *         in row 0 has neighbors in the last row if a cell is in the same
+	 *         column, or the column to the left or right. In this example, cell 0,5
+	 *         has two neighbors in the last row, cell 2,8 has four neighbors, cell
+	 *         2,0 has four neighbors, cell 1,0 has three neighbors. The cell at 3,8
+	 *         has 3 neighbors. The potential location for a cell at 4,8 would have
+	 *         three neighbors.
+	 */
+	// .....O..O
+	// O........
+	// O.......O
+	// O.......O
+	// ....O.O..
+	public int neighborCount(int row, int col) {
+		int count = 0;
+
+		if (row == rows - 1 || col == cols - 1) {
+			if (row == rows - 1 && col == cols - 1) {// bottom right corner
+				count += grid[row - 1][col - 1];
+				count += grid[row - 1][col];
+				count += grid[row - 1][0];
+				count += grid[row][col - 1];
+				count += grid[row][0];
+				count += grid[0][0];
+				count += grid[0][col];
+				count += grid[0][col - 1];
+			} else if (row == 0 && col == cols - 1) {// top right
+				count += grid[rows - 1][col - 1];
+				count += grid[rows - 1][col];
+				count += grid[rows - 1][0];
+				count += grid[row][col - 1];
+				count += grid[row][0];
+				count += grid[row + 1][col - 1];
+				count += grid[row + 1][col];
+				count += grid[row + 1][0];
+			} else if (row == rows - 1 && col == 0) {// bottom left
+				count += grid[row - 1][cols - 1];
+				count += grid[row - 1][col];
+				count += grid[row - 1][col + 1];
+				count += grid[row][cols - 1];
+				count += grid[row][col + 1];
+				count += grid[0][cols - 1];
+				count += grid[0][col];
+				count += grid[0][col + 1];
+			} else if (row == rows - 1) { // last row
+				count += grid[row - 1][col - 1];
+				count += grid[row - 1][col];
+				count += grid[row - 1][col + 1];
+				count += grid[row][col - 1];
+				count += grid[row][col + 1];
+				count += grid[0][col - 1];
+				count += grid[0][col];
+				count += grid[0][col + 1];
+			} else { // last column
+				count += grid[row - 1][col - 1];
+				count += grid[row - 1][col];
+				count += grid[row - 1][0];
+				count += grid[row][col - 1];
+				count += grid[row][0];
+				count += grid[row + 1][col - 1];
+				count += grid[row + 1][col];
+				count += grid[row + 1][0];
+
+			}
+		} else if (row == 0 || col == 0) {
+			if (row == 0 && col == 0) {// top left corner
+				count += grid[rows - 1][cols - 1];
+				count += grid[rows - 1][col];
+				count += grid[rows - 1][col + 1];
+				count += grid[row][cols - 1];
+				count += grid[row][col + 1];
+				count += grid[row + 1][cols - 1];
+				count += grid[row + 1][col];
+				count += grid[row + 1][col + 1];
+			} else if (row == 0) { // top
+				count += grid[rows - 1][col - 1];
+				count += grid[rows - 1][col];
+				count += grid[rows - 1][col + 1];
+				count += grid[row][col - 1];
+				count += grid[row][col + 1];
+				count += grid[row + 1][col - 1];
+				count += grid[row + 1][col];
+				count += grid[row + 1][col + 1];
+			} else {// left side
+				count += grid[row - 1][cols - 1];
+				count += grid[row - 1][col];
+				count += grid[row - 1][col + 1];
+				count += grid[row][cols - 1];
+				count += grid[row][col + 1];
+				count += grid[row + 1][cols - 1];
+				count += grid[row + 1][col];
+				count += grid[row + 1][col + 1];
+
+			}
+		} else {
+			count += grid[row - 1][col - 1];
+			count += grid[row - 1][col];
+			count += grid[row - 1][col + 1];
+			count += grid[row][col - 1];
+			count += grid[row][col + 1];
+			count += grid[row + 1][col - 1];
+			count += grid[row + 1][col];
+			count += grid[row + 1][col + 1];
+		}
+
+		return count;
+	}
+
+	public void update() {
+		int[][] gridCopy = new int[rows][cols];
+
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				gridCopy[r][c] = grid[r][c];
+				if (grid[r][c] == 0 && neighborCount(r, c) == 3) {
+					gridCopy[r][c] = 1;
+				} else if (grid[r][c] == 1 && (neighborCount(r, c) < 2 || neighborCount(r, c) > 3)) {
+					gridCopy[r][c] = 0;
+				}
+			}
+		}
+		grid = gridCopy;
+
+	}
 }
--- GameOfLifeTest.java
+++ GameOfLifeTest.java
@@ -1,40 +1,172 @@
+
+//Jesse Pelletier
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GameOfLifeTest {
-  @Test
-  public void testConstructorAndGetters() {
-    GameOfLife society = new GameOfLife(5, 8);
-    assertEquals(5, society.numberOfRows());
-    assertEquals(8, society.numberOfColumns());
-    for (int r = 0; r < society.numberOfRows(); r++)
-      for (int c = 0; c < society.numberOfColumns(); c++)
-        assertFalse(society.cellAt(r, c));
-  }
+	@Test
+	public void testConstructorAndGetters() {
+		GameOfLife society = new GameOfLife(5, 8);
+		assertEquals(5, society.numberOfRows());
+		assertEquals(8, society.numberOfColumns());
+		for (int r = 0; r < society.numberOfRows(); r++)
+			for (int c = 0; c < society.numberOfColumns(); c++)
+				assertFalse(society.cellAt(r, c));
+	}
 
-  @Test
-  public void testGrowCellAtAndCellAt() {
-    GameOfLife society = new GameOfLife(4, 4);
-    society.growCellAt(1, 1);
-    society.growCellAt(2, 2);
-    society.growCellAt(3, 3);
-    assertTrue(society.cellAt(1, 1));
-    assertTrue(society.cellAt(2, 2));
-    assertTrue(society.cellAt(3, 3));
-  }
+	@Test
+	public void testGrowCellAtAndCellAt() {
+		GameOfLife society = new GameOfLife(4, 4);
+		society.growCellAt(1, 1);
+		society.growCellAt(2, 2);
+		society.growCellAt(3, 3);
+		assertTrue(society.cellAt(1, 1));
+		assertTrue(society.cellAt(2, 2));
+		assertTrue(society.cellAt(3, 3));
+	}
 
-  @Test
-  public void testNeighborsWrapping() {
-    GameOfLife society = new GameOfLife(10, 16);
-    society.growCellAt(3, 3);
-    society.growCellAt(3, 4);
-    society.growCellAt(3, 5);
-    assertEquals(0, society.neighborCount(2, 1));
-    assertEquals(1, society.neighborCount(2, 2));
-    assertEquals(2, society.neighborCount(2, 3));
-    assertEquals(3, society.neighborCount(2, 4));
-    // ... many more assertions expected
-  }
+	@Test
+	public void testNeighbors() {
+		GameOfLife society = new GameOfLife(7, 11);
+		society.growCellAt(3, 3);
+		society.growCellAt(3, 4);
+		society.growCellAt(3, 5);
+		assertEquals(0, society.neighborCount(2, 1));
+		assertEquals(1, society.neighborCount(2, 2));
+		assertEquals(2, society.neighborCount(2, 3));
+		assertEquals(3, society.neighborCount(2, 4));
+		assertEquals(2, society.neighborCount(2, 5));
+		assertEquals(1, society.neighborCount(2, 6));
 
-  // ... Add many more @Test methods with many, many assertions here
+		// corners
+
+	}
+
+	@Test
+	public void testNeighborsWraparound() {
+		GameOfLife society = new GameOfLife(7, 11);
+
+		society.growCellAt(0, 0);
+		society.growCellAt(2, 0);
+		society.growCellAt(0, 10);
+		society.growCellAt(2, 10);
+		society.growCellAt(6, 0);
+		society.growCellAt(6, 10);
+		assertEquals(2, society.neighborCount(1, 1));
+		assertEquals(2, society.neighborCount(0, 1));
+		assertEquals(4, society.neighborCount(1, 0));
+		assertEquals(2, society.neighborCount(0, 9));
+		assertEquals(2, society.neighborCount(5, 10));
+		assertEquals(2, society.neighborCount(6, 9));
+		assertEquals(2, society.neighborCount(6, 1));
+
+		// corners
+		assertEquals(3, society.neighborCount(0, 0));
+		assertEquals(3, society.neighborCount(6, 10));
+		assertEquals(3, society.neighborCount(0, 10));
+		assertEquals(3, society.neighborCount(6, 0));
+
+	}
+
+	@Test
+	public void testToString() {
+		GameOfLife society = new GameOfLife(5, 5);
+
+		String outputStr = society.toString();
+		assertEquals(".....\n.....\n.....\n.....\n.....", outputStr);
+
+		society.growCellAt(0, 0);
+		society.growCellAt(1, 1);
+		society.growCellAt(2, 2);
+		society.growCellAt(3, 3);
+		society.growCellAt(4, 4);
+
+		outputStr = society.toString();
+
+		assertEquals("O....\n.O...\n..O..\n...O.\n....O", outputStr);
+
+	}
+
+	@Test
+	public void testUpdateMicro() {
+		GameOfLife society = new GameOfLife(3, 3);
+
+		society.update();
+
+		// assert empty
+		for (int r = 0; r < society.numberOfRows(); r++) {
+			for (int c = 0; c < society.numberOfColumns(); c++) {
+				assertFalse(society.cellAt(r, c));
+			}
+		}
+
+		society.growCellAt(1, 1);
+
+		society.update(); // cell should die off
+
+		// assert empty
+		for (int r = 0; r < society.numberOfRows(); r++) {
+			for (int c = 0; c < society.numberOfColumns(); c++) {
+				assertFalse(society.cellAt(r, c));
+			}
+		}
+
+		society.growCellAt(0, 1);
+		society.growCellAt(1, 0);
+		society.growCellAt(1, 2);
+		society.growCellAt(2, 1);
+
+		society.update();
+
+		assertFalse(society.cellAt(0, 0));
+		assertTrue(society.cellAt(0, 1));
+		assertFalse(society.cellAt(0, 2));
+		assertTrue(society.cellAt(1, 0));
+		assertFalse(society.cellAt(1, 1));
+		assertTrue(society.cellAt(1, 2));
+		assertFalse(society.cellAt(2, 0));
+		assertTrue(society.cellAt(2, 1));
+		assertFalse(society.cellAt(2, 2));
+
+	}
+
+	@Test
+	public void testUpdateMacro() {
+		GameOfLife society = new GameOfLife(7, 7);
+
+		society.growCellAt(0, 0);
+		society.growCellAt(0, 6);
+		society.growCellAt(2, 2);
+		society.growCellAt(3, 2);
+		society.growCellAt(4, 4);
+		society.growCellAt(6, 6);
+		society.growCellAt(2, 1);
+		society.growCellAt(3, 1);
+		society.growCellAt(4, 1);
+		society.growCellAt(6, 5);
+
+		society.update();
+
+		// alive
+		assertTrue(society.cellAt(0, 6));
+		assertTrue(society.cellAt(1, 0));
+		assertTrue(society.cellAt(1, 1));
+		assertTrue(society.cellAt(2, 1));
+		assertTrue(society.cellAt(2, 2));
+		assertTrue(society.cellAt(3, 0));
+		assertTrue(society.cellAt(3, 3));
+		assertTrue(society.cellAt(4, 1));
+		assertTrue(society.cellAt(4, 2));
+		assertTrue(society.cellAt(5, 5));
+		assertTrue(society.cellAt(6, 5));
+		assertTrue(society.cellAt(6, 6));
+		assertTrue(society.cellAt(0, 0));
+		assertTrue(society.cellAt(6, 0));
+
+		// dead
+		assertFalse(society.cellAt(3, 1));
+		assertFalse(society.cellAt(3, 2));
+		assertFalse(society.cellAt(4, 4));
+
+	}
 }
